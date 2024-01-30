214a460a78e1a86243ed1f8ccda4007fcfe4dfeb
Igor Feliphe <igufeliphe@gmail.com>
2024-01-29 23:39:17 -0300

    update
    update my blog

--- pag/home.php
+++
@@ -1,7 +0,0 @@
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">Pagina Inicial.</h1>
-        <p class="lead">Diretorio Git.</p>
-        <p class="lead">
-          <a href="https://github.com/igufeliphe/igufeliphe.github.io" target="_ablank" class="btn btn-lg btn-secondary">Saiba mais</a>
-        </p>
-      </main>
